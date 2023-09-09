6bb52fac874a39717335bf5b3ca122e0a409dfe9
Wesley Moraes <wesleycrz@hotmail.com>
2023-09-09 19:42:01 -0300

    insercao no banco de dados

--- src/App.js
+++ src/App.js
@@ -23,16 +23,67 @@ function App() {
   const [todos, setTodos] = useState([]) //as proprias tarefas e imprimir elas
   const [loading, setLoadin] = useState(false) //Feedback pro usuario
 
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+   
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false
+    };
+
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type" : "application/json",
+      }
+    })
+
+    //Enviar para a API
+    console.log(todo)
+    setTitle("")
+    setTime("")
+  };
+
+
   return(
     <div className="App">
       <div className="todo_header">
         <h1>React To Do</h1>
       </div>
-      <div className="form-todo">
-        <p>Formulário</p>
+      <div className="form_todo">
+        <h2>Insira sua próxima tarefa: </h2>
+        <form onSubmit={handleSubmit}>
+          <div className='form_control'>
+            <label htmlFor='title'>O que você vai fazer?</label>
+            <input 
+              type='text' 
+              name='title' 
+              placeholder='Título da terefa'
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required
+            /> 
+          </div>
+          <div className='form_control'>
+            <label htmlFor='time'>Duração: </label>
+            <input 
+              type='text' 
+              name='time' 
+              placeholder="Tempo estimado (em horas)"
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
+              required
+            /> 
+          </div>
+          <input type='submit' value="Enviar" />
+        </form>
       </div>
-      <div className="list-todo">
-        <p>Lista</p>
+      <div className="list_todo">
+        <h2>Lista de tarefas: </h2>
+        {todos.length === 0 && <p>Não há tarefas</p>}
       </div>
       {/*
       <h2>Hello react</h2>
